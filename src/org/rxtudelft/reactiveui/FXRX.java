46aee5ee50e896dbf280f7120fed92dd71d00f8d
Jeff Smits <jeff.smits@gmail.com>
2014-05-16 16:17:40 +0200

    Added fromScene, analogous to fromNode. Added direct subscribing versions.

--- src/org/rxtudelft/reactiveui/FXRX.java
+++ src/org/rxtudelft/reactiveui/FXRX.java
@@ -4,7 +4,9 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.Event;
 import javafx.event.EventType;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import rx.Observable;
+import rx.Observer;
 
 /**
  * Wrappers for easily transitioning from JavaFX to RxJava
@@ -14,10 +16,26 @@ public class FXRX {
     public static <T extends Event> Observable<T> fromNode(EventType<T> eventType, Node node) {
         return Observable.create((Observable.OnSubscribe<T>) s -> node.addEventHandler(eventType, s::onNext));
     }
+
+    public static <T extends Event> Observable<T> fromScene(EventType<T> eventType, Scene scene) {
+        return Observable.create((Observable.OnSubscribe<T>) s -> scene.addEventHandler(eventType, s::onNext));
+    }
     
     public static <T> Observable<T> fromObservableValue(ObservableValue<T> observableValue) {
         return Observable.create((Observable.OnSubscribe<T>) s -> {
             observableValue.addListener((obs, oldValue, newValue) -> s.onNext(newValue));
         });
+    }
+
+    public static <T extends Event> void subscribeNode(EventType<T> eventType, Node node, Observer<? super T> observer) {
+        node.addEventHandler(eventType, observer::onNext);
+    }
+
+    public static <T extends Event> void subscribeScene(EventType<T> eventType, Scene scene, Observer<? super T> observer) {
+        scene.addEventHandler(eventType, observer::onNext);
+    }
+
+    public static <T> void subscribeObservableValue(ObservableValue<T> observableValue, Observer<? super T> observer) {
+        observableValue.addListener((obs, oldValue, newValue) -> observer.onNext(newValue));
     }
 }
